aead2a33a64e96a0ba42277cd3f9468a4258495c
Ville Svärd <ville@agical.se>
2011-02-07 22:41:33 +0100

    First, stack-overflowing, attempt

--- src/challange/core.clj
+++ src/challange/core.clj
@@ -1,5 +1,22 @@
 (ns challange.core
   (:require [clojure.contrib.str-utils2 :as s]))
 
+(def challange "VOCDIITEIOCRUDOIANTOCSLOIOCVESTAIOCVOLIOCENTSU")
+
+(def words (map str '(TDD, DDD, DI, DO, OO, UI, ANT, CV, IOC, LOC, SU, VO)))
+
 (defn remove-from [s pattern]
-  (s/replace s pattern ""0))
+  (s/replace s pattern ""))
+
+(defn remove-all [s patterns]
+  (let [candidates (filter #(s/contains? s %) patterns)]
+    (map #(vector (vector %) (remove-from s %)) candidates)))
+
+
+(defn reduce-further [so-far patterns]
+  (loop [[[path s] & rest :as reductions] so-far]
+    (let [candidates (filter #(s/contains? s %) patterns)
+          applied (map #(vector (conj path %) (remove-from s %)) candidates)]
+      (if (and (seq candidates) (> 200 (count reductions)))
+        (recur (concat rest applied))
+        reductions))))
--- test/challange/test/core.clj
+++ test/challange/test/core.clj
@@ -5,3 +5,12 @@
 (fact "removes patterns from a string"
       (remove-from "ABA" "A") => "B"
       (remove-from "ABA" "B") => "AA")
+
+;.;. Before the reward there must be labor. You plant before you
+;.;. harvest. You sow in tears before you reap joy. -- Ransom
+(fact
+ (remove-all "ABBA" ["A" "B"]) => [[["A"] "BB"] [["B"] "AA"]]
+ (remove-all "ABBA" ["A" "C"]) => [[["A"] "BB"]])
+
+(fact
+ ( reduce-further [["A"] "CBBC"] ["C"]) => [[["A" "C"] "BB"]])
