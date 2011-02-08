39691453ca5a19a695ce0c23e853503b09ef15e7
Ville Svärd <ville@agical.se>
2011-02-08 21:54:21 +0100

    First restructuring. Committing before another panicky, bug-ridden change

--- src/challange/core.clj
+++ src/challange/core.clj
@@ -8,15 +8,24 @@
 (defn remove-from [s pattern]
   (s/replace s pattern ""))
 
+(defn contained-in [patterns string]
+  (filter #(s/contains? string %) patterns))
+
 (defn remove-all [s patterns]
-  (let [candidates (filter #(s/contains? s %) patterns)]
+  (let [candidates  (contained-in patterns s)]
     (map #(vector (vector %) (remove-from s %)) candidates)))
+
+(defn apply-candidate-path [path s candidate]
+  (vector (conj path candidate) (remove-from s candidate)))
+
+(defmacro dbg[x] `(let [x# ~x] (println "dbg:" '~x "=" x#) x#))
 
 
 (defn reduce-further [so-far patterns]
   (loop [[[path s] & rest :as reductions] so-far]
-    (let [candidates (filter #(s/contains? s %) patterns)
-          applied (map #(vector (conj path %) (remove-from s %)) candidates)]
+    (let [candidates (contained-in patterns s)
+          applied (map (partial apply-candidate-path path s) candidates)]
       (if (and (seq candidates) (> 200 (count reductions)))
         (recur (concat rest applied))
         reductions))))
+
--- test/challange/test/core.clj
+++ test/challange/test/core.clj
@@ -2,15 +2,23 @@
   (:use [challange.core] :reload)
   (:use [midje.sweet]))
 
+(fact
+ (contained-in ["A" "BB"] "ABBA") => ["A" "BB"]
+ (contained-in ["BB" "CC"] "AAA") => [])
+
 (fact "removes patterns from a string"
       (remove-from "ABA" "A") => "B"
       (remove-from "ABA" "B") => "AA")
 
-;.;. Before the reward there must be labor. You plant before you
-;.;. harvest. You sow in tears before you reap joy. -- Ransom
 (fact
  (remove-all "ABBA" ["A" "B"]) => [[["A"] "BB"] [["B"] "AA"]]
  (remove-all "ABBA" ["A" "C"]) => [[["A"] "BB"]])
 
+;.;. Effort only fully releases its reward after a person refuses to
+;.;. quit. -- Hill
 (fact
- ( reduce-further [["A"] "CBBC"] ["C"]) => [[["A" "C"] "BB"]])
+ ( reduce-further [[["A"] "CBBC"]] ["C"]) => [[["A" "C"] "BB"]])
+
+
+(fact
+ (apply-candidate-path ["A"] "BBCC" "C") => [["A" "C"] "BB"])
