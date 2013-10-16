8a67e983825d82522f11a3ae6a754dc3049fb81b
Ville Svärd <ville.svard@agical.com>
2013-10-16 21:06:34 +0200

    asdf

--- project.clj
+++ project.clj
@@ -2,5 +2,5 @@
   :description "FIXME: write"
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]]
-  :dev-dependencies [[swank-clojure "1.2.0"]
+  :dev-dependencies [
                      [midje "1.0.1"]])
